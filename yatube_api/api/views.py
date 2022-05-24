99df81ab62490dd71568037d961cac602cee4587
gagai <gagai.vlad@gmail.com>
2022-05-24 13:13:50 +0300

    author is autoadded now when post creating

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -7,6 +7,9 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+        
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
